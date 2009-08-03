1d47d034b1929b93c7504a6f20eb9dbc2e464039
Glen Maddern <glen83@gmail.com>
2009-08-04 00:18:17 +1000

    got my pro on, step.json added as example

--- src/main/scala/Gitwatch.scala
+++ src/main/scala/Gitwatch.scala
@@ -33,10 +33,19 @@ object Gitwatch {
       val head = repo.mapCommit("HEAD")
       val his: List[Commit] = history(repo)(head)
       val mappedHistory = his.map((c: Commit) => {
-	    Map("id" -> c.getCommitId.name)
+	    Map("id" -> c.getCommitId.name,
+            "parents" -> List.fromArray(c.getParentIds.toArray).map(_.name),
+            "timestamp" -> c.getAuthor.getWhen.getTime / 1000,
+            "y" -> 0,
+            "author" -> c.getAuthor.getName,
+            "committer" -> c.getCommitter.getName,
+            "message" -> c.getMessage,
+            "size" -> 0.0
+        )
       })
 /*      println(mappedHistory)*/
-      mappedHistory.jsonString
+      val json : String = Map("data" -> mappedHistory).jsonString
+      json
     } catch {
       case e => e.toString
     }
