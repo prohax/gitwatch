267e6b096d071269114748ee921646101543fc1a
Glen Maddern <glen83@gmail.com>
2009-08-03 22:37:32 +1000

    Jgit is in and working oh baby

--- src/main/scala/Gitwatch.scala
+++ src/main/scala/Gitwatch.scala
@@ -1,9 +1,24 @@
+import java.io.{IOException, File}
+import org.spearce.jgit.lib.Repository
+import collection.jcl.Conversions._
+
+case class JsonCommit(id: String, )
 
 /**
  * TODO: Class comment.
  */
 object Gitwatch {
-  def toJson(gitDir: String) : String = {
-    "lol"
+  def toJson(gitDir: String): String = {
+    try {
+      val g = new File(gitDir)
+      val repo = new Repository(g)
+      val refs = repo.getAllRefs
+
+      val head = repo.mapCommit("HEAD")
+      println(head)
+      refs.toString
+    } catch {
+      case e => e.toString
+    }
   }
 }
--- src/test/scala/GitwatchTest.scala
+++ src/test/scala/GitwatchTest.scala
@@ -7,8 +7,9 @@ import org.specs._
 object GitwatchTest extends Specification {
   "gitwatch main" should {
     "list the branches of a simple project" in {
-      Gitwatch.toJson("~/src/step/.git") must_!= ""
-//      Gitwatch.toJson("~/src/step/.git") must_!= "lol"
+      val step = Gitwatch.toJson("/Users/glen/src/step/.git")
+      println(step)
+      step must_!= ""
     }
   }
 }
