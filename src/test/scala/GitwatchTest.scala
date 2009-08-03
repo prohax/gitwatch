0d41522203ee829e182b32ef376c85c2c816f160
Glen Maddern <glen83@gmail.com>
2009-08-03 20:10:17 +1000

    stubbing out some resources

--- src/test/scala/GitwatchTest.scala
+++ src/test/scala/GitwatchTest.scala
@@ -4,21 +4,11 @@
  */
 import org.specs._
 
-object helloWorld extends Specification {
-  "hello world" should {
-    "have 11 characters" in {
-      "hello world".size must_== 11
-    }
-    "match 'h.* w.*'" in {
-      "hello world" must be matching("h.* w.*")
-    }
-  }
-}
-
 object GitwatchTest extends Specification {
   "gitwatch main" should {
     "list the branches of a simple project" in {
       Gitwatch.toJson("~/src/step/.git") must_!= ""
+//      Gitwatch.toJson("~/src/step/.git") must_!= "lol"
     }
   }
 }
