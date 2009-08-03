d02a19a29e55da24fd823963bceafa1bf660b3b6
Glen Maddern <glen83@gmail.com>
2009-08-04 00:00:32 +1000

    YEAH BITCHES

--- src/main/scala/Gitwatch.scala
+++ src/main/scala/Gitwatch.scala
@@ -1,13 +1,29 @@
 import java.io.{IOException, File}
-import org.spearce.jgit.lib.Repository
 import collection.jcl.Conversions._
+import org.spearce.jgit.lib.{Commit, Repository}
+import scapps.JSON
+import scapps.Id._
 
-case class JsonCommit(id: String, )
+case class JsonCommit(id: String,
+                      parents: List[String],
+                      timestamp: Int,
+                      y: Int,
+                      author: String,
+                      committer: String,
+                      message: String,
+                      size: Float)
 
 /**
  * TODO: Class comment.
  */
 object Gitwatch {
+  def history(repo: Repository)(c: Commit): List[Commit] =
+    List.fromArray(c.getParentIds).map(repo.mapCommit(_)) match {
+      case Nil => c :: Nil
+      case parents => c :: parents.flatMap(history(repo)(_))
+    }
+
+
   def toJson(gitDir: String): String = {
     try {
       val g = new File(gitDir)
@@ -15,8 +31,12 @@ object Gitwatch {
       val refs = repo.getAllRefs
 
       val head = repo.mapCommit("HEAD")
-      println(head)
-      refs.toString
+      val his: List[Commit] = history(repo)(head)
+      val mappedHistory = his.map((c: Commit) => {
+	    Map("id" -> c.getCommitId.name)
+      })
+/*      println(mappedHistory)*/
+      mappedHistory.jsonString
     } catch {
       case e => e.toString
     }
