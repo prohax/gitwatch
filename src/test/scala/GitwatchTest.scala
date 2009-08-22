59a62aa39d6544ce4767b5f26cac7afcbeaa2ecf
Glen Maddern <glen83@gmail.com>
2009-08-23 02:20:42 +1000

    oh yeah!!

--- src/test/scala/GitwatchTest.scala
+++ src/test/scala/GitwatchTest.scala
@@ -3,8 +3,8 @@ import org.specs._
 object GitwatchTest extends Specification {
   "gitwatch main" should {
     "list the branches of a simple project" in {
-      val step = Gitwatch.toJson("/Users/glen/src/babushka/.git")
-      println(step)
+      val step = Gitwatch.toJson("/Users/glen/src/gitwatch/.git")
+//      println(step)
       step must_!= ""
     }
   }
