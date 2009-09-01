86985a1d6d3e25aec588f7f4644d136218b07602
Glen Maddern <glen83@gmail.com>
2009-09-01 17:23:33 +1000

    dumb java process forking is dumb

--- src/test/scala/GitwatchTest.scala
+++ src/test/scala/GitwatchTest.scala
@@ -6,5 +6,17 @@ object GitwatchTest extends Specification {
       val step = Gitwatch.toJson(".git")
       step must_!= ""
     }
+    "test" in {
+      val p = Runtime.getRuntime.exec("git rev-list HEAD")
+      val in = p.getInputStream
+      var ch = in.read
+      val sb = new StringBuffer(512)
+      while (ch != -1) {
+        sb.append(ch.asInstanceOf[Char])
+        ch = in.read
+      }
+      println(sb.toString)
+      sb.toString must_!= ""
+    }
   }
 }
