56d6890e2e8e47605b0b67dc130cb62031c91c74
Ben Hoskings <ben@hoskings.net>
2009-09-02 14:09:47 +1000

    Refactored Gitwatch.

--- src/main/scala/Gitwatch.scala
+++ src/main/scala/Gitwatch.scala
@@ -4,42 +4,39 @@ import org.spearce.jgit.lib.{Commit => GitCommit, Repository}
 import scapps.JSON
 import scapps.Id._
 
-case class Commit(id: String,
-                  parents: List[String],
-                  timestamp: Int,
-                  y: Int,
-                  author: String,
-                  committer: String,
-                  message: String,
-                  size: Float)
-
 object Gitwatch {
-  def toJson(gitDir: String): String = {
-    val g = new File(gitDir)
-    val repo = new Repository(g)
-    val refs = repo.getAllRefs
-
-    val head = repo.mapCommit("HEAD")
-    val his: List[GitCommit] = Traverser.history(repo, head)
-    val gitCommits = his.foldLeft(Map[String, GitCommit]())((m, c) => m + (c.getCommitId.name -> c))
-    val graph = Grapher.graph(his.map((c) => Node(
+  private def graphedHistory(repo: Repository, head: GitCommit, hist: List[GitCommit]) = {
+    Grapher.graph(hist.map((c) => Node(
       c.getCommitId.name,
       List.fromArray(c.getParentIds.toArray).map(_.name),
-      c.getAuthor.getWhen.getTime / 1000)), head.getCommitId.name)
-//    println(repo.openObject(repo.resolve("HEAD")).getBytes)
-    val mappedHistory = graph.map((g: GraphedNode) => {
-      Map("id" -> g.node.id,
+      c.getAuthor.getWhen.getTime / 1000)), head.getCommitId.name
+    )
+  }
+
+  private def mappedGraph(repo: Repository, head: GitCommit) = {
+    val hist = Traverser.history(repo, head)
+    val commits = hist.foldLeft(Map[String, GitCommit]())((m, c) => m + (c.getCommitId.name -> c))
+
+    graphedHistory(repo, head, hist).map((g: GraphedNode) => {
+      Map(
+        "id" -> g.node.id,
         "parents" -> g.node.parents,
         "timestamp" -> g.node.timestamp,
         "y" -> g.y,
-        "author" -> gitCommits(g.node.id).getAuthor.getName,
-        "committer" -> gitCommits(g.node.id).getCommitter.getName,
-        "message" -> gitCommits(g.node.id).getMessage.replaceAll("\n", "\\n"),
+        "author" -> commits(g.node.id).getAuthor.getName,
+        "committer" -> commits(g.node.id).getCommitter.getName,
+        "message" -> commits(g.node.id).getMessage.replaceAll("\n", "\\n"),
         "size" -> repo.openObject(repo.resolve(g.node.id)).getBytes.length
-        )
+      )
     })
-    /*      println(mappedHistory)*/
-    val json: String = Map("data" -> mappedHistory).jsonString
-    json
+  }
+
+  def toJson(gitDir: String): String = {
+    val repo = new Repository(new File(gitDir))
+    val head = repo.mapCommit("HEAD")
+
+    Map("data" -> (
+      if (head == null) "" else mappedGraph(repo, head)
+    )).jsonString
   }
 }
