751cb598857de57e0c99fec372ede700d2356290
Glen Maddern <glen83@gmail.com>
2009-09-02 18:00:20 +1000

    started down scapps route, current build of scalaz doesnt include scapps :(

--- project/build/GitwatchProject.scala
+++ project/build/GitwatchProject.scala
@@ -4,12 +4,14 @@ class GitwatchProject(info: ProjectInfo) extends DefaultWebProject(info) {
   import BasicScalaProject._
 
   override def jettyPort = 1337
+  override def scanDirectories = ( temporaryWarPath / "WEB-INF" / "classes" ).get.toSeq
 /*  override def useMavenConfigurations = true*/
 /*  override def packageAction = packageTask(mainClasses +++ mainResources, outputPath, defaultJarName, packageOptions).dependsOn(compile) describedAs PackageDescription*/
 /*  override def scanDirectories = ( temporaryWarPath / "WEB-INF" / ("classes" | "lib") ).get.map(_.asFile).toSeq*/
 
   val jetty2 = "org.mortbay.jetty" % "jetty" % "7.0.0pre3" % "test->default"
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
+
 /*  val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "7.0.0pre3" % "test->default"*/
 /*  val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"*/
 }
---
+++ src/main/scala/GitwatchScappsServlet.scala
@@ -0,0 +1,8 @@
+//import scalaz.http.scapps.
+// oh fail.
+/**
+ * TODO: Class comment.
+ */
+//class GitwatchScappsServlet extends BaseApp {
+//
+//}
