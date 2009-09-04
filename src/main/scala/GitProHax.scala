299f20ef144f955f91248c4beee0443501d745ad
Glen Maddern <glen83@gmail.com>
2009-09-04 16:10:32 +1000

    got branches happening too

--- src/main/scala/GitProHax.scala
+++ src/main/scala/GitProHax.scala
@@ -1,23 +1,27 @@
+import collection.immutable.TreeMap
 import collection.mutable.{ArrayBuffer, HashSet, Queue, HashMap}
-import collection.jcl.{HashMap => JclHashMap}
 import java.io.File
 import org.spearce.jgit.lib.{Commit => GitCommit, Repository}
+import collection.jcl.{Map => JclMap}
 
 case class GraphedCommit(c: GitCommit, y: Int)
 
 class GitProHax(repo: Repository) {
-  val seen = new HashSet[String]
-  val graph = new ArrayBuffer[GraphedCommit]
+  val seen = new HashMap[String, GraphedCommit]
   val heightMap = new HeightMap
   //needs to be a priority queue
   val mergesQueue = new Queue[(String, Int)]
 
-  def graph(head: GitCommit, branches: Seq[GitCommit]): List[GraphedCommit] = {
+  def graph(head: GitCommit, branches: List[GitCommit]): List[GraphedCommit] = {
     renderBack(head, 0)
     println(heightMap)
     mergesQueue.foreach(t => renderBack(repo.mapCommit(t._1), t._2))
     println(heightMap)
-    graph.toList
+
+    val branchesByBranchTime = new TreeMap[Long,GitCommit]() ++ branches.map(b => (timeOf(findSeenParent(b)), b))
+    println(branchesByBranchTime)
+
+    seen.values.toList.sort((a,b) => timeOf(a.c) < timeOf(b.c))
   }
 
   def renderBack(c: GitCommit, y: Int) {
@@ -28,8 +32,7 @@ class GitProHax(repo: Repository) {
         case Nil => ()
         case head :: tail => {
           mergesQueue ++= tail.map((_, y + 1))
-          seen += id
-          graph += GraphedCommit(c, y)
+          seen += (id -> GraphedCommit(c, y))
           val parent = repo.mapCommit(head)
           heightMap.add(y, timeOf(parent), timeOf(c))
           renderBack(parent, y)
@@ -38,13 +41,17 @@ class GitProHax(repo: Repository) {
     }
   }
 
+  def findSeenParent(c: GitCommit) : GitCommit = {
+    c
+  }
+
   private def timeOf(c: GitCommit) = c.getCommitter.getWhen.getTime / 1000
 }
 
 object GitProHax {
   def run(gitDir: String, master: String) = {
     val repo = new Repository(new File(gitDir))
-    val branches = collection.jcl.Map(repo.getAllRefs)
+    val branches = JclMap(repo.getAllRefs)
     val head = repo.mapCommit(branches(master).getObjectId)
     val graphedCommits = new GitProHax(repo).graph(head, branches.values.map(x => repo.mapCommit(x.getObjectId)).toList)
     graphedCommits.map(g => (g.c.getCommitId.name.substring(0,7), g.y)).mkString("\n")
